c7390a38afe89173cf42e79e19cc7ab2808d2e25
browser-vm <namelessonbandlab@outlook.com>
2025-02-14 00:48:55 +0000

    rebuild with chemical

---
+++ index.js
@@ -0,0 +1,16 @@
+import { ChemicalServer } from "chemicaljs";
+import express from "express";
+
+const [app, listen] = new ChemicalServer();
+const port = process.env.PORT || 3000;
+
+app.use(express.static("public", {
+    index: "index.html",
+    extensions: ["html"]
+}));
+
+app.serveChemical();
+
+listen(port, () => {
+    console.log(`LoveHeart listening on port ${port}`);
+});
