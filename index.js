28644fb85e3f207dfaa0bcf4527cde82226c5560
browser-vm <namelessonbandlab@outlook.com>
2025-03-14 12:04:50 +0000

    completely remove torrent features

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
 import { ChemicalServer } from "chemicaljs";
 import express from "express";
-import WebTorrent from 'webtorrent';
-
-const client = new WebTorrent({
-  tracker: {
-    iceServers: [
-      {
-        urls: [
-          'stun:stun.l.google.com:19302',
-          'stun:stun1.l.google.com:19302',
-          'stun:stun2.l.google.com:19302',
-          'stun:stun3.l.google.com:19302',
-          'stun:stun4.l.google.com:19302'
-        ]
-      }
-    ]
-  }
-});
 
 const [app, listen] = new ChemicalServer({
   bypassDownloads: true,
@@ -28,23 +11,6 @@ const [app, listen] = new ChemicalServer({
     'application/x-msdownload',
     'application/x-apple-diskimage'
   ]
-});
-
-// Add endpoint for torrent downloads
-app.post('/torrent', express.json(), (req, res) => {
-  const { magnetUri } = req.body;
-  
-  client.add(magnetUri, { announceList: [] }, (torrent) => {
-    torrent.on('done', () => {
-      console.log('Torrent download finished');
-    });
-    
-    res.json({
-      infoHash: torrent.infoHash,
-      name: torrent.name,
-      progress: torrent.progress
-    });
-  });
 });
 
 const port = process.env.PORT || 3000;
